b2b6313d2105b46631df4c5fe04c417ed5c74343
Simon King <simon.king@stronans.com>
2020-10-16 20:33:52 +0100

    Updated Title links and styles, added scratch pads to all puzzles except taxi.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,16 @@ import {CountdownEvent} from "ngx-countdown";
 })
 
 export class AppComponent implements OnInit {
+  asset = '../assets/title1.PNG';
   title = 'Can you escape from Zombie Glasgow?';
   private timerService: TimerControlService;
+  config = {
+    demand: true,
+    leftTime: 20,      // 1800,
+    notify: [10],     // 300
+    format: 'm:ss'
+  }
+  extraDetail = '';
 
   @ViewChild('cd', {static: false}) private countdown: CountdownComponent;
 
@@ -28,14 +36,46 @@ export class AppComponent implements OnInit {
           this.countdown.stop();
         }
       });
+
+    switch (AppComponent.getRndInteger(0, 2)) {
+      case 0 :
+        this.asset = '../assets/title1.PNG';
+        break;
+      case 1 :
+        this.asset = '../assets/title2.PNG';
+        break;
+      case 2 :
+        this.asset = '../assets/title3.PNG';
+        break;
+    }
   }
 
   ngOnInit(): void {
   }
 
+  private static getRndInteger(min, max): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   handleEvent(event: CountdownEvent) {
     if (event.action == "stop") {
-      this.timerService.update(event.left)
+      this.timerService.update(event.left);
     }
+
+    if (event.action == "done") {
+      AppComponent.changeElement("mainBody");
+      AppComponent.changeElement("mainhtml");
+      this.asset = '../assets/title4.png';
+      this.extraDetail = '';
+    }
+
+    if (event.action == "notify") {
+      this.extraDetail = 'Less than 5 minutes to go';
+    }
+  }
+
+  private static changeElement(id): void {
+    var el = document.getElementById(id);
+    el.style.backgroundColor = "#590039";
   }
 }
